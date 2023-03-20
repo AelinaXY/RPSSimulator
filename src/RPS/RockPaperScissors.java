0f9509f4d2f087c64205ac8371035ce75fb8d102
Lauren Darlaston <80410139+AelinaXY@users.noreply.github.com>
2023-03-20 20:01:12 +0000

    Change method to be worse

--- src/RPS/RockPaperScissors.java
+++ src/RPS/RockPaperScissors.java
@@ -7,17 +7,10 @@ public class RockPaperScissors {
 	private Random rand = new Random();
 
 	public String rpsFight(validRPSMoves userChoice) {
-		int rand = this.rand.nextInt(3);
-
-		System.out.println(validRPSMoves.values()[rand]);
-
-		return (validRPSMoves.values()[rand] != userChoice)
-				? ((userChoice == validRPSMoves.PAPER && validRPSMoves.values()[rand] == validRPSMoves.ROCK)
-						|| (userChoice == validRPSMoves.ROCK && validRPSMoves.values()[rand] == validRPSMoves.SCISSORS)
-						|| (userChoice == validRPSMoves.SCISSORS && validRPSMoves.values()[rand] == validRPSMoves.PAPER)
-								? "WIN"
-								: "LOSS")
-				: "DRAW";
-	}
-
+		validRPSMoves n;
+		return   ((n = validRPSMoves.values()[this.rand.nextInt(3)])  != userChoice)
+			? ((userChoice == validRPSMoves.PAPER && n == validRPSMoves.ROCK)
+			|| (userChoice == validRPSMoves.ROCK && n == validRPSMoves.SCISSORS)
+			|| (userChoice == validRPSMoves.SCISSORS && n == validRPSMoves.PAPER)
+			? "WIN": "LOSS"): "DRAW";}
 }
